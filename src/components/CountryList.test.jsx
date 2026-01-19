43d92e267ec09a94026deb786853804b566ab6f0
Kasper Kouhia <60173278+kasperkouhia@users.noreply.github.com>
2026-01-19 18:34:23 +0200

    Add tests for CountryList

---
+++ src/components/CountryList.test.jsx
@@ -0,0 +1,42 @@
+import { render, screen } from "@testing-library/react";
+import { within } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { describe, it, expect, beforeEach, vi } from "vitest";
+import CountryList from "./CountryList";
+
+const countries = [
+  { name: { common: "Finland" } },
+  { name: { common: "Sweden" } },
+  { name: { common: "Norway" } },
+];
+
+const mockSetter = vi.fn();
+
+describe("CountryList", () => {
+  beforeEach(() =>
+    render(<CountryList countries={countries} countrySetter={mockSetter} />),
+  );
+
+  it("Renders a list of countries", () => {
+    for (const country of countries) {
+      expect(screen.getByText(country.name.common)).toBeInTheDocument();
+    }
+
+    expect(screen.getAllByRole("listitem")).toHaveLength(countries.length);
+  });
+
+  it("Calls countrySetter correctly on button press", async () => {
+    const user = userEvent.setup();
+
+    for (const country of countries) {
+      const countryListItem = screen
+        .getByText(country.name.common)
+        .closest("li");
+      const countryButton = within(countryListItem).getByRole("button");
+
+      await user.click(countryButton);
+
+      expect(mockSetter).toHaveBeenCalledWith(country.name.common);
+    }
+  });
+});
