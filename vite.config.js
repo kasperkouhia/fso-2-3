fb4cbb43dc7f1849d93474adba3bfea655db1fbd
Kasper Kouhia <60173278+kasperkouhia@users.noreply.github.com>
2026-01-19 16:05:39 +0200

    Configure vitest

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  test: {
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.js",
+  },
   plugins: [react(), tailwindcss()],
 });
